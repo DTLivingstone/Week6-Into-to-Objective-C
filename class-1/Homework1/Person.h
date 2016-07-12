bbcea3510e7184f32d3c5382d3eff61d50db2101
David Livingstone <david@david.tl>
2016-07-12 09:00:00 -0700

    problem 3

---
+++ class-1/Homework1/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  Homework1
+//
+//  Created by David Livingstone on 7/12/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ class-1/Homework1/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Homework1
+//
+//  Created by David Livingstone on 7/12/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
