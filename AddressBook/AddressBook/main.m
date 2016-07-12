54f26d696c10e92f148397edee44b63be3f9ed68
David Livingstone <david@david.tl>
2016-07-12 13:26:41 -0700

    created project

---
+++ AddressBook/AddressBook/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AddressBook
+//
+//  Created by David Livingstone on 7/12/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
