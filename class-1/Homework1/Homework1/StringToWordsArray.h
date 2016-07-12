a1cb4e2a62ad55d7087be7ce328397776aac09b1
David Livingstone <david@david.tl>
2016-07-12 08:46:12 -0700

    first two problems finished

---
+++ class-1/Homework1/Homework1/StringToWordsArray.h
@@ -0,0 +1,15 @@
+//
+//  StringToWords.h
+//  Homework1
+//
+//  Created by David Livingstone on 7/11/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (StringToWordsArray)
+
++(NSArray *) toWords: (NSString *) input;
+
+@end
---
+++ class-1/Homework1/Homework1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework1
+//
+//  Created by David Livingstone on 7/11/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
