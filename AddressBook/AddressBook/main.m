67852a5cce45bc19be561c056b3aca2881a90d0d
David Livingstone <david@david.tl>
2016-07-13 13:44:18 -0700

    created project

--- AddressBook/AddressBook/main.m
+++ AddressBook/AddressBook/main.m
@@ -2,32 +2,15 @@
 //  main.m
 //  AddressBook
 //
-//  Created by David Livingstone on 7/12/16.
+//  Created by David Livingstone on 7/13/16.
 //  Copyright © 2016 David Livingstone. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "DLUserDefaults.h"
-#import "Contact.h"
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, char * argv[]) {
     @autoreleasepool {
-//        [[DLUserDefaults sharedDefaults]setObject:@(sender.isOn) forKey:@"com.michaelbabiy.kUserPreferences"];
-        
-        Contact *david = [[Contact alloc]init];
-        
-        [david setName:@"David Livingstone"];
-        [david setAddress:@"123 Main Street, Seattle Wa, 98103"];
-        [david setPhone:@"575-555-3204" ];
-        [david setEmail:@"david@david.tl"];
-        [david setTwitter:@"@livingstone86"];
-        
-        NSLog(@"Name: %@", david.name);
-        NSLog(@"Address: %@", david.name);
-        NSLog(@"Phone: %@", david.name);
-        NSLog(@"Email: %@", david.name);
-        NSLog(@"Twitter: %@", david.name);
-        
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
-    return 0;
 }
