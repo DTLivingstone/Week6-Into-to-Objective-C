ec83c06ca442c652c321b8dd57e1487d9ff8b39c
David Livingstone <david@david.tl>
2016-07-13 23:41:43 -0700

    work on view controllers

---
+++ AddressBook/AddressBook/AddContactViewController.m
@@ -0,0 +1,42 @@
+//
+//  AddContactViewController.m
+//  AddressBook
+//
+//  Created by David Livingstone on 7/13/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+#import "AddContactViewController.h"
+//#import "Contact+Extension.h"
+//#import "Store.h"
+
+@interface AddContactViewController ()
+
+@property (strong, nonatomic) Contact *contact;
+
+// ib outlets
+
+@end
+
+
+@implementation AddContactViewController
+
+
+
+
+
+
+
+
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
